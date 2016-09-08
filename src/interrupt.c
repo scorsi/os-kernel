d733bf4057a5092a8766bdc2beff1aabe940de61
SylvainCorsini <sylvain.corsini@gmail.com>
2016-09-08 13:59:09 +0200

    [Evolution] Better handler of interupts of keyboard tahnks to chipset 8042.

--- src/interrupt.c
+++ src/interrupt.c
@@ -17,5 +17,54 @@ void isr_clock_int(void) {
 }
 
 void isr_kbd_int(void) {
-    print("keyboard\n");
+    uchar i;
+    static int lshift_enable;
+    static int rshift_enable;
+    static int alt_enable;
+    static int ctrl_enable;
+
+    do {
+        i = inb(0x64);
+    } while ((i & 0x01) == 0);
+
+    i = inb(0x60);
+    i--;
+
+    //// putcar('\n'); dump(&i, 1); putcar(' ');
+
+    if (i < 0x80) {         /* touche enfoncee */
+        switch (i) {
+            case 0x29:
+                lshift_enable = 1;
+                break;
+            case 0x35:
+                rshift_enable = 1;
+                break;
+            case 0x1C:
+                ctrl_enable = 1;
+                break;
+            case 0x37:
+                alt_enable = 1;
+                break;
+            default:
+                putcar(kbdmap[i * 4 + (lshift_enable || rshift_enable)]);
+        }
+    } else {                /* touche relachee */
+        i -= 0x80;
+        switch (i) {
+            case 0x29:
+                lshift_enable = 0;
+                break;
+            case 0x35:
+                rshift_enable = 0;
+                break;
+            case 0x1C:
+                ctrl_enable = 0;
+                break;
+            case 0x37:
+                alt_enable = 0;
+                break;
+        }
+    }
+    show_cursor();
 }
